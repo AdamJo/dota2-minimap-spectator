657da86e21d4794d0e100508e1b89934e2234d1b
Adam Johannesmeyer <adamj0101@gmail.com>
2016-08-26 15:28:11 -0700

    updated live game model

--- src/client/app/services/liveLeagueGame.ts
+++ src/client/app/services/liveLeagueGame.ts
@@ -2,48 +2,39 @@
 
 export interface LiveLeagueGame {
   $key?: string;
-  dire_series_wins: number;
   dire_team_name: string;
-  game_number: number;
   league: {
     name: string,
     tournament_url: string
   };
-  league_game_id: number;
-  league_series_id: number;
-  league_tier: string;
   match_id: number;
-  radiant_series_wins: number;
   radiant_team_name: string;
   scoreboard: {
     day_cycle: string,
+    did_game_start: boolean,
     dire: {
-      abilities: Array<Abilities>,
-      bans: Array<Bans>,
-      barracks_state: number,
-      picks: Array<Picks>,
+      // abilities: Array<Abilities>,
+      bans: Array<string>,
+      barracks_state: string,
+      picks: Array<string>,
       players: Array<Players>,
       score: number,
-      tower_state: number
+      tower_state: string
     },
-    did_game_start: boolean,
     duration: number,
     radiant: {
-      abilities: Array<Abilities>,
-      bans: Array<Bans>,
-      barracks_state: number,
-      picks: Array<Picks>,
+      // abilities: Array<Abilities>,
+      bans: Array<string>,
+      barracks_state: string,
+      picks: Array<string>,
       players: Array<Players>,
       score: number,
-      tower_state: number
+      tower_state: string
     },
     roshan_respawn_timer: number
   };
-  series_id: number;
   series: Series;
-  series_type: string;
   spectators: number;
-  stage_name: string;
   stream_delay_s: number;
 }
 
@@ -53,39 +44,39 @@ export interface Abilities {
 }
 
 export interface Bans {
-  hero_id : number;
+  hero_id : string;
 }
 
 export interface Picks {
-  hero_id : number;
+  hero_id : string;
 }
 
 export interface Series {
-  dire_series_wins: number;
-  radiant_series_wins: number;
+  dire_series_wins: Array<number>;
+  radiant_series_wins: Array<number>;
 }
 
 export interface Players {
-  ultimate_cooldown: number;
-  xp_per_min: number;
-  position_x: number;
-  gold_per_min: number;
-  last_hits: number;
-  assists: number;
-  ultimate_state: number;
-  respawn_timer: number;
-  items: Array<number>;
-  name: string;
-  death: number;
-  gold: number;
-  net_worth: number;
   account_id: number;
-  position_y: number;
-  kills: number;
-  player_slot: number;
-  level: number;
-  hero_id: number;
+  assists: number;
+  death: number;
   denies: number;
+  gold: number;
+  gold_per_min: number;
+  hero: string;
+  items: Array<string>;
+  kills: number;
+  last_hits: number;
+  level: number;
+  name: string;
+  net_worth: number;
+  player_slot: number;
+  position_x: number;
+  position_y: number;
+  respawn_timer: number;
   old_position_x?: number;
   old_position_y?: number;
+  ultimate_cooldown: number;
+  ultimate_state: number;
+  xp_per_min: number;
 }
