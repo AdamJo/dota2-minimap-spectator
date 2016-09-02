753b64ad9ef2e58b62ee7a5ac8fa2ec82712ecba
Adam Johannesmeyer <adamj0101@gmail.com>
2016-09-02 16:26:05 -0700

    added abyssal underloard pixel art

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { LiveGameComponent } from './live-game/index';
-import { UpcomingMatchesComponent } from './upcoming-matches/index';
+// import { UpcomingMatchesComponent } from './upcoming-matches/index';
 
 
 export const routes: Routes = [
   { path: '', component: LiveGameComponent },
-  { path: 'upcoming', component: UpcomingMatchesComponent },
+  // { path: 'upcoming', component: UpcomingMatchesComponent },
   { path: '**', component: LiveGameComponent }
 ];
