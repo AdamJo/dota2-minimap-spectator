71102c8a315bd6e7c8072f9721c28b58a7124f68
Adam Johannesmeyer <adamj0101@gmail.com>
2016-09-12 13:57:02 -0700

    fix(apiService) remove debug

--- src/client/app/services/api.service.ts
+++ src/client/app/services/api.service.ts
@@ -43,7 +43,6 @@ export class ApiService {
       if (this.upcomingMatches.length === 0) {
         this.upcomingMatches = testData;
       }
-      console.log(this.upcomingMatches);
     });
   }
 
