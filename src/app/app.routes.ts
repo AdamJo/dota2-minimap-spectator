9df4b07a239ff69173b2a7cca0ca9a9b30ec4be7
Adam Johannesmeyer <adamj0101@gmail.com>
2016-09-15 16:12:05 -0700

    chore(cleanup) remove Live Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { UpcomingMatchesComponent } from './upcoming-matches/index';
 
 export const routes: Routes = [
   { path: '', component: LiveGameComponent },
-  { path: 'Live', component: LiveGameComponent },
   { path: 'Upcoming', component: UpcomingMatchesComponent },
   { path: '**', component: LiveGameComponent }
 ];
