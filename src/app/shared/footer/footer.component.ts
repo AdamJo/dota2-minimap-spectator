29497089d96acd4d81631ec40b0c6b2b217f5162
Adam Johannesmeyer <adamj0101@gmail.com>
2016-09-14 18:25:45 -0700

    chore(footer) add for info for footer.
    footer now shows SteamAPI and DailyDota link info.

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -4,22 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   template: `
     <footer>
-      Made by <a href="https://github.com/AdamJo">AdamJo</a>
+      Minimal Dota 2 Watcher by <a target="_blank" href="https://github.com/AdamJo">AdamJo</a>
+      | Live Game Info by <a href="https://wiki.teamfortress.com/wiki/WebAPI#Dota_2" target="_blank">SteamApi</a>
+      | Upcoming Matches Info by <a href="http://dailydota2.com/api" target="_blank">DailyDota</a>
     </footer>
   `,
   styles: [
     `
       a {
         color: #d9d9d9;
-
-        &:active {
-          color: #142958;
-        }
-
-        &:visited {
-          color: #22627E;
-        }
       }
+
+      a:visited {
+        color: #22627E;
+      }
+
+      a:active {
+        color: #142958;
+      }
+
       footer {
         background: #000;
         color: #d9d9d9;
