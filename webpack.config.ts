e266bc715ba6ac08860e582c5f232d9c4a74b8f7
Adam Johannesmeyer <adamj0101@gmail.com>
2016-10-07 13:35:04 -0700

    chore() update from upstream

--- webpack.config.ts
+++ webpack.config.ts
@@ -64,7 +64,6 @@ const DLL_VENDORS = [
   '@angular/core',
   '@angular/platform-browser',
   '@angular/platform-browser-dynamic',
-  '@angular/platform-server',
   '@angular/router',
   'rxjs',
   ...MY_VENDOR_DLLS
@@ -245,43 +244,3 @@ const clientConfig = function webpackConfig(): WebpackConfig {
 
 DLL ? console.log('BUILDING DLLs') : console.log('BUILDING APP');
 module.exports = clientConfig;
-
-// // Types
-interface WebpackConfig {
-  cache?: boolean;
-  target?: string;
-  devtool?: string;
-  entry: any;
-  externals?: any;
-  output: any;
-  module?: any;
-  plugins?: Array<any>;
-  resolve?: {
-    extensions?: Array<string>;
-  };
-  devServer?: {
-    contentBase?: string;
-    port?: number;
-    historyApiFallback?: boolean;
-    hot?: boolean;
-    inline?: boolean;
-    proxy?: any;
-    host?: string;
-    quiet?: boolean;
-    noInfo?: boolean;
-    watchOptions?: any;
-  };
-  node?: {
-    process?: boolean;
-    global?: boolean;
-    Buffer?: boolean;
-    crypto?: boolean;
-    module?: boolean;
-    clearImmediate?: boolean;
-    setImmediate?: boolean
-    clearTimeout?: boolean;
-    setTimeout?: boolean;
-    __dirname?: boolean;
-    __filename?: boolean;
-  };
-}
