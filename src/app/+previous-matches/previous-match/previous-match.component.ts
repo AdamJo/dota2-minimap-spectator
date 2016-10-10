cd361ac8d87c6d117d88c43346f4188315ff465b
Adam Johannesmeyer <adamj0101@gmail.com>
2016-10-09 19:41:55 -0700

    design(previous) split columns into two and show winner

--- src/app/+previous-matches/previous-match/previous-match.component.ts
+++ src/app/+previous-matches/previous-match/previous-match.component.ts
@@ -9,4 +9,20 @@ import { MatchHistory } from '../../services/index'
 })
 export class PreviousMatchComponent {
   @Input() matches: any;
+  splitMatches: any;
+
+  constructor() {
+    this.splitMatches = [];
+  }
+
+  ngOnChanges(changes) {
+    if (this.matches.length > 26) {
+      this.splitMatches = this.matches.slice(26, 52)
+      this.matches = this.matches.slice(0,26)
+      console.log(this.splitMatches.length)
+      console.log(this.matches.length)
+    } else {
+      this.splitMatches = []
+    }
+  }
 }
