76ae1e5c478162bf0f1e41edc2aa9cd9991b8d60
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-10 23:52:15 -0800

    feature() add messingSenderId for angularfire2

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -6,7 +6,8 @@ const firebaseConfig = {
   apiKey: 'AIzaSyAgULOLZZOd5IHc5ABgOIm8_dTsrunyYRs',
   authDomain: 'dota2-project-c0fd5.firebaseapp.com',
   databaseURL: 'https://dota2-project-c0fd5.firebaseio.com',
-  storageBucket: 'dota2-project-c0fd5.appspot.com'
+  storageBucket: 'dota2-project-c0fd5.appspot.com',
+  messagingSenderId: "158994092281"
 };
 
 export const APP_IMPORTS = [
